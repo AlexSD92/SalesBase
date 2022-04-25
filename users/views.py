8c0d986e0257bfcc85188945804c7e77066af41d
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-04-25 20:51:33 +0100

    RegisterUserView created to handle user registration data. If form method is post and valid, form data is saved

--- users/views.py
+++ users/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 def LoginView(request):
     return render(request, 'users/login.html')
+
+def RegisterUserView(request):
+    if request.method =='POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return render(request, 'users/login.html')
+        messages.error(request, 'Unsuccessful registration. Invalid information.')
+    form = RegisterUserForm()
+    return render(request, 'users/register.html', context={'RegisterForm':form})
