c20bca9d5e51cc5a87f1027bee14bcaba42872e1
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-01 05:34:03 +0100

    Cleanup of imports

--- users/views.py
+++ users/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render, redirect
 from .forms import CustomUserForm
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
-from django.contrib import messages
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 
-class RegisterUserView(CreateView):
+class RegisterUserView(CreateView,):
     form_class = CustomUserForm
     success_url = reverse_lazy('login')
     template_name = 'users/register.html'
