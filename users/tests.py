f41faf5fd8c0ba0cdba6fbf12762ea1dba7bbaab
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-01 05:33:00 +0100

    URL tests for register, form tests for register

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RegisterPageTests(TestCase):
+
+    def test_register_url_exists_at_correct_location_registeruserview(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_user_view_name(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_register_user_form(self):
+        response = self.client.post(reverse('register'),
+            {
+                'username':'testuser',
+                'role':'test',
+                'password1':'AVeryComplicatedPswd123',
+                'password2':'AVeryComplicatedPswd123',
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'testuser')
+        self.assertEqual(get_user_model().objects.all()[0].role, 'test')
