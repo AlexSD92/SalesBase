37670925b7afc0c1639765ab160e60fa8914f3a8
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-03 20:09:57 +0100

    Authenitcation required added

--- salesbase/urls.py
+++ salesbase/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
+from accounts.views import DashboardAccountsListView
 
 
 urlpatterns = [
@@ -26,6 +27,9 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('', auth_views.LoginView.as_view(template_name='users/login.html',redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('dashboard/', login_required(TemplateView.as_view(template_name='dashboard.html')), name='dashboard'),
+    path('dashboard/', login_required(DashboardAccountsListView.as_view()), name='dashboard'),
+    # path('dashboard/', login_required(TemplateView.as_view(template_name='dashboard.html')), name='dashboard'),
     path('accounts/', include('accounts.urls'), name='accounts'),
 ]
+
+    # path('', AccountsListView.as_view(), name='account-list-view'),
