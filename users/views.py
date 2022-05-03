ff8884ffdb876c7ce0426e5d255baed11a798152
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-03 20:11:42 +0100

    Syntax error where comma was included in arguments corrected

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 
-class RegisterUserView(CreateView,):
+class RegisterUserView(CreateView):
     form_class = CustomUserForm
     success_url = reverse_lazy('login')
     template_name = 'users/register.html'
