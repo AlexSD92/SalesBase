914986d29f9980f8f0d5103e66cf6958e95b727f
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-03 21:37:16 +0100

    Removed commented out code and unused paths

--- salesbase/urls.py
+++ salesbase/urls.py
@@ -28,8 +28,5 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html',redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('dashboard/', login_required(DashboardAccountsListView.as_view()), name='dashboard'),
-    # path('dashboard/', login_required(TemplateView.as_view(template_name='dashboard.html')), name='dashboard'),
     path('accounts/', include('accounts.urls'), name='accounts'),
 ]
-
-    # path('', AccountsListView.as_view(), name='account-list-view'),
