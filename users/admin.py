de4c1ad76dc089ff1708d566576f5e7d5cc3f1f7
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-08 20:34:46 +0100

    Removed all previous CustomUserModel code and registered Profile model

--- users/admin.py
+++ users/admin.py
@@ -1,22 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from .models import Profile
 
-from .forms import CustomUserForm, CustomUserUpdateForm
-from .models import CustomUserModel
-
-class CustomUserAdmin(UserAdmin):
-    add_form=CustomUserForm
-    form=CustomUserUpdateForm
-    model=CustomUserModel
-    list_display=[
-        'username',
-        'first_name',
-        'last_name',
-        'email',
-        'role',
-        'is_staff',
-    ]
-    fieldsets=UserAdmin.fieldsets +((None,{'fields':('role',)}),)
-    add_fieldsets=UserAdmin.add_fieldsets +((None,{'fields':('role',)}),)
-
-admin.site.register(CustomUserModel, CustomUserAdmin)
+admin.site.register(Profile)
