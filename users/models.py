33fe62b1e17121d6a0bd3284aab50d8060161546
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-08 20:59:08 +0100

    Profile view created

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-class CustomUserModel(AbstractUser):
-    role = models.CharField(max_length=40,null=True,blank=False)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return f'{self.user}'
