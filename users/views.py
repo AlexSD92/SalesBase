4ef934c873294490ebaf42acda04b4a99bdfafe8
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-08 21:04:17 +0100

    RegisterUserView created, Profile view created

--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
-from .forms import CustomUserForm
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import CreateNewUser
 
 
-class RegisterUserView(CreateView):
-    form_class = CustomUserForm
-    success_url = reverse_lazy('login')
-    template_name = 'users/register.html'
+def RegisterUserView(request):
+    if request.method == 'POST':
+        form = CreateNewUser(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('login')
+    else:
+        form = CreateNewUser()
+    return render(request, 'users/register.html', {'form': form})
+
+def Profile(request):
+    return render(request, 'users/profile.html')
+
+
