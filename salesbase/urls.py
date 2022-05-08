8fe73825159d760c0305509038b8a75f7ebec2d7
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-08 20:29:52 +0100

    Profile imported from users.views, added RegisterUserView path

--- salesbase/urls.py
+++ salesbase/urls.py
@@ -13,19 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from users.views import RegisterUserView
+from users.views import RegisterUserView, Profile
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from accounts.views import DashboardAccountsListView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', RegisterUserView.as_view(), name='register'),
+    path('register/', RegisterUserView, name='register'),
     path('', auth_views.LoginView.as_view(template_name='users/login.html',redirect_authenticated_user=True), name='login'),
+    path('profile/', login_required(Profile), name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('dashboard/', login_required(DashboardAccountsListView.as_view()), name='dashboard'),
     path('accounts/', include('accounts.urls'), name='accounts'),
