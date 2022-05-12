680943d62f70a332e5a0f68c4802044963e71f63
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-12 20:22:52 +0100

    ContactsModel created to include contact-name, contact_company, contact_role, contact_opp, created, updated and owner

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ContactsModel(models.Model):
+    contact_name = models.CharField(max_length=50, null=True, blank=False)
+    contact_company = models.CharField(max_length=50, null=True, blank=False)
+    contact_role = models.CharField(max_length=50, null=True, blank=False)
+    contact_opp = models.CharField(max_length=50, null=True, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contact_name
