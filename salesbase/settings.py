d15fd4ee7f4d64fdde4f3600cfdd43eeb8d12f60
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-12 19:21:30 +0100

    Contacts.apps.ContactsConfig added to INSTALLED_APPS

--- salesbase/settings.py
+++ salesbase/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'contacts.apps.ContactsConfig',
     'accounts.apps.AccountsConfig',
     'whitenoise.runserver_nostatic',
     'crispy_forms',
