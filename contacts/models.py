6f4f7d270508303d94da2a449bb9b475555c8ed9
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-18 20:09:39 +0100

    Added verbose names to ContactsModel and fields. Contact_company given one-to-many relationship with AccountsModel. Contact_opp given one-to-many relationship with OpportunitiesModel

--- contacts/models.py
+++ contacts/models.py
@@ -1,16 +1,22 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from accounts.models import AccountsModel
+from opportunities.models import OpportunitiesModel
 
 
 class ContactsModel(models.Model):
-    contact_name = models.CharField(max_length=50, null=True, blank=False)
-    contact_company = models.CharField(max_length=50, null=True, blank=False)
-    contact_role = models.CharField(max_length=50, null=True, blank=False)
-    contact_opp = models.CharField(max_length=50, null=True, blank=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    contact_name = models.CharField(max_length=50, null=True, blank=False, verbose_name='Contact Name')
+    contact_company = models.ForeignKey(AccountsModel, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Contact Company')
+    contact_role = models.CharField(max_length=50, null=True, blank=False, verbose_name='Contact Role')
+    contact_opp = models.ForeignKey(OpportunitiesModel, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Contact Opportunity')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Last Updated')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner')
 
     def __str__(self):
         return self.contact_name
+
+    class Meta:
+        verbose_name='Contact'
