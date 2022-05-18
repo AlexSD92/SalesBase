5d476e6ac8992624a8c03648d47058f051781454
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-18 20:14:30 +0100

    Updated OpportunitiesModel ti include many-to-one relationship with AccountsModel for account_name. Verbose names added for model and fields

--- opportunities/models.py
+++ opportunities/models.py
@@ -1,18 +1,21 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 from accounts.models import AccountsModel
 
 
 class OpportunitiesModel(models.Model):
-    opportunity_name = models.CharField(max_length=50, null=True, blank=False)
-    opportunity_company = models.CharField(max_length=50, null=True, blank=False)
-    # account_name = models.OneToOneField(AccountsModel, on_delete=models.CASCADE)
-    # opportunity_value = models.IntegerField(null=True, blank=False)
-    # opportunity_close_date = models.DateField(null=True, blank=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    opportunity_name = models.CharField(max_length=50, null=True, blank=False, verbose_name='Opportunity Name')
+    account_name = models.ForeignKey(AccountsModel, on_delete=models.CASCADE, verbose_name='Opportunity Account')
+    opportunity_value = models.PositiveIntegerField(null=True, blank=False, verbose_name='Opportunity Value')
+    # opportunity_contacts = models.ManyToManyField(ContactsModel, on_delete=models.CASCADE, verbose_name='Opportunity Contacts')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Last Updated')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner')
 
     def __str__(self):
         return self.opportunity_name
+
+    class Meta:
+        verbose_name='Opportunitie'
