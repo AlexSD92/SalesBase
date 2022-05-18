11575b40bd951296dc26f9200e1ac79534ccd641
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-05-18 20:06:10 +0100

    Imported the user model and updated the owner field to include the User model. Added verbose names to all model fields. Added Meta class with verbose name for admin view

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,19 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 class AccountsModel(models.Model):
-    account_name = models.CharField(max_length=50, null=True, blank=False)
-    account_ind = models.CharField(max_length=50, null=True, blank=False)
-    account_size = models.PositiveIntegerField(null=True, blank=False)
-    account_rev = models.PositiveIntegerField(null=True, blank=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=50, null=True, blank=False, unique=True, verbose_name='Account Name')
+    account_ind = models.CharField(max_length=50, null=True, blank=False, verbose_name='Account Industry')
+    account_size = models.PositiveIntegerField(null=True, blank=False, verbose_name='# of Employees')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Created On')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Last Updated')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner')
 
     def __str__(self):
         return self.account_name
+
+    class Meta:
+        verbose_name='Account'
