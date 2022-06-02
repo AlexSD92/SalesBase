1d4aee35728d52b308986833531f0eb63b37d6ce
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-06-02 20:54:56 +0100

    DJMoney added, moneyfield included in opportunities model

--- opportunities/models.py
+++ opportunities/models.py
@@ -4,12 +4,13 @@ from django.urls import reverse
 from accounts.models import AccountsModel
 from salesbase.list_data import OPP_STATUS_CHOICES
 from django.utils.text import slugify
+from djmoney.models.fields import MoneyField
 
 
 class OpportunitiesModel(models.Model):
     opportunity_name = models.CharField(max_length=50, null=True, blank=False, verbose_name='Opportunity Name')
     account_name = models.ForeignKey(AccountsModel, on_delete=models.CASCADE, verbose_name='Opportunity Account')
-    opportunity_value = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=False, verbose_name='Opportunity Value')
+    opportunity_value = MoneyField(max_digits=20, decimal_places=2, null=True, blank=False, default_currency='GBP', verbose_name='Opportunity Value')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
     updated = models.DateTimeField(auto_now=True, verbose_name='Last Updated')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner')
--- salesbase/settings.py
+++ salesbase/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'djmoney',
 ]
 
 # AUTH_USER_MODEL = 'users.CustomUserModel'
